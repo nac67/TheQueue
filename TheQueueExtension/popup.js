492c2ac6ed80af44308e9ac6df6abd904a322758
Aaron Nelson <asn62@cornell.edu>
2014-09-05 20:51:49 -0400

    got some basic queue stuff working

--- TheQueueExtension/popup.js
+++ TheQueueExtension/popup.js
@@ -1,9 +1,17 @@
 function blah (data) {
-  var txt = data.url;
-  var a = document.getElementById("thisthing");
-  a.innerHTML = txt;
-}
+  var queue = Cookies.get("queue");
+  if (queue === undefined) {
+    queue = [];
+  } else {
+    queue = JSON.parse(queue);
+  }
+  queue.push(data.url);
 
+  var queue_as_string = JSON.stringify(queue);
+  Cookies.set("queue", queue_as_string);
+  var a = document.getElementById("thisthing");
+  a.innerHTML = queue_as_string;
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   NATabTools.getTab(blah);
